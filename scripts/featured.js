494aeb6798cd962acc477297aa7417d9047654b9
Abdul1091 <abdul2kur1@gmail.com>
2024-08-26 17:06:19 +0100

    First commit

---
+++ scripts/featured.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMCOntentLoaded', function() {
+	// JavaScript code for featured services can be added here
+	// For now, it's a static section
+});
---
+++ scripts/how_it_works.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMCOntentLoaded', function() {
+	// JavaScript code for how it works section can be added here
+	// For now, it's a static section
+});
